c8acd2a187ae797e8170823ef8c6a44b5e2fa2a7
LimenitisNS <LimenitisNS@users.noreply.github.com>
2020-10-15 19:20:10 +0700

    LimenitisNS
    Added hot reload

--- configs/webpack.common.js
+++ configs/webpack.common.js
@@ -1,13 +1,9 @@
+const paths = require('./paths');
+
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const paths = {
-    src: path.resolve(__dirname, '../src'), // source files
-    build: path.resolve(__dirname, '../build'), // production build files
-    static: path.resolve(__dirname, '../public'), // static files to copy to build folder
-};
 
 module.exports = {
     entry: [paths.src + '/index.js'],
@@ -26,6 +22,17 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: paths.static + '/index.html', // template file
             filename: 'index.html', // output file
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+            {
+                from: paths.static,
+                to: paths.build,
+                    // добавилось нижеследующее
+                globOptions: {
+                    ignore: ['**/index.html'],
+                },
+            },],
         }),
     ],
     module: {
