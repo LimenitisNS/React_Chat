21b35cf813532b256891089bd41436618460ce46
LimenitisNS <LimenitisNS@users.noreply.github.com>
2020-11-10 17:17:56 +0700

    Added validation for login and registration forms
    Added Formik

--- src/Views/LoginView/LoginView.js
+++ src/Views/LoginView/LoginView.js
@@ -1,69 +1,93 @@
 import React from "react";
 import APIService from "../../APIService";
 import styles from "./styles.module.css";
+import { Formik } from "formik";
 
 export default class LoginView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      nickname: "",
-      password: "",
       result: null,
       error: null
     };
   }
 
-  hendleSubmit(event) {
-    const { nickname, password } = this.state;
-
-    this.setState({
-      result: null,
-      error: null
-    });
-
+  handleSubmit(values) {
     APIService.auth
-      .login({
-        nickname: nickname,
-        password: password
-      })
+      .login(values)
       .then(() => {
-        this.setState({ result: "The user has successfully logged in" });
+        this.setState({
+          result: "The user has successfully logged in"
+        });
         setTimeout(() => this.props.history.push("profile"), 2000);
       })
-      .catch((error) => this.setState({ error: "Error" + error.response.data.error }));
-
-    event.preventDefault();
+      .catch((error) => this.setState({ error: "Error " + error.response.data.error }));
   }
 
   render() {
-    const { error, result, nickname, password } = this.state;
+    const { error, result } = this.state;
     return (
       <>
-        <div>
-          <h1>Login</h1>
-          {error}
-          {result}
-          <form onSubmit={(e) => this.hendleSubmit(e)}>
-            <div className={styles.login}>
-              <label>Nickname</label>
-              <input
-                type="text"
-                value={nickname}
-                className={styles.login_input}
-                onChange={(e) => this.setState({ nickname: e.target.value })}
-              />
-            </div>
-            <div className={styles.login}>
-              <label>Password</label>
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => this.setState({ password: e.target.value })}
-              />
-            </div>
-            <button type="submit">Enter</button>
-          </form>
-        </div>
+        <h1>Login</h1>
+        <div className={styles.error}>{error}</div>
+        {result}
+        <Formik
+          initialValues={{ nickname: "", password: "" }}
+          validate={(values) => {
+            const errors = {};
+
+            if (!values.nickname) {
+              errors.nickname = "Enter a nickname";
+            }
+            if (!values.password) {
+              errors.password = "Enter a password";
+            }
+            if (values.password.length < 7) {
+              errors.password = "The password must be longer than 6 characters";
+            }
+            if (values.nickname.length < 4) {
+              errors.nickname = "The nickname must be longer than 3 characters";
+            }
+
+            return errors;
+          }}
+          onSubmit={(values) => {
+            this.handleSubmit(values);
+          }}
+        >
+          {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <div className={styles.login}>
+                <label>Nickname</label>
+                <input
+                  type="text"
+                  name="nickname"
+                  value={values.nickname}
+                  className={styles.login_input}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <div className={styles.error}>
+                  {errors.nickname && touched.nickname && errors.nickname}
+                </div>
+              </div>
+              <div className={styles.login}>
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <div className={styles.error}>
+                  {errors.password && touched.password && errors.password}
+                </div>
+              </div>
+              <button type="submit">Enter</button>
+            </form>
+          )}
+        </Formik>
       </>
     );
   }
--- src/Views/RegistrationView/RegistrationView.js
+++ src/Views/RegistrationView/RegistrationView.js
@@ -1,72 +1,94 @@
 import React from "react";
 import APIService from "../../APIService";
 import styles from "./styles.module.css";
+import { Formik } from "formik";
 
 export default class RegistrationView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      nickname: "",
-      password: "",
       result: null,
       error: null
     };
   }
 
-  handleSubmit(event) {
-    const { nickname, password } = this.state;
-    this.setState({
-      result: null,
-      error: null
-    });
-
+  handleSubmit(values) {
     APIService.user
-      .create({
-        nickname: nickname,
-        password: password
-      })
+      .create(values)
       .then(() => {
         this.setState({
           result: "User successfully registered"
         });
-
         setTimeout(() => this.props.history.push("/login"), 2000);
       })
-      .catch((error) => this.setState({ error: "Error" + error.response.data.error }));
-
-    event.preventDefault();
+      .catch((error) => this.setState({ error: "Error " + error.response.data.error }));
   }
 
   render() {
-    const { nickname, password } = this.state;
+    const { error, result } = this.state;
     return (
       <>
         <h1>Registration</h1>
-        {this.state.error}
-        {this.state.result}
-        <div>
-          <form onSubmit={(e) => this.handleSubmit(e)}>
-            <div className={styles.registration}>
-              <label>Nick</label>
-              <input
-                type="text"
-                value={nickname}
-                className={styles.registration_input_nick}
-                onChange={(e) => this.setState({ nickname: e.target.value })}
-              />
-            </div>
-            <div className={styles.registration}>
-              <label>Password</label>
-              <input
-                type="password"
-                value={password}
-                className={styles.registration_input_password}
-                onChange={(e) => this.setState({ password: e.target.value })}
-              />
-            </div>
-            <button type="submit">Registration</button>
-          </form>
-        </div>
+        <div className={styles.error}>{error}</div>
+        {result}
+        <Formik
+          initialValues={{ nickname: "", password: "" }}
+          validate={(values) => {
+            const errors = {};
+
+            if (!values.nickname) {
+              errors.nickname = "Enter a nickname";
+            }
+            if (!values.password) {
+              errors.password = "Enter a password";
+            }
+            if (values.password.length < 7) {
+              errors.password = "The password must be longer than 6 characters";
+            }
+            if (values.nickname.length < 4) {
+              errors.nickname = "The nickname must be longer than 3 characters";
+            }
+
+            return errors;
+          }}
+          onSubmit={(values) => {
+            this.handleSubmit(values);
+          }}
+        >
+          {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <div className={styles.registration}>
+                <label>Nick</label>
+                <input
+                  type="text"
+                  name="nickname"
+                  value={values.nickname}
+                  className={styles.registration_input_nick}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <div className={styles.error}>
+                  {errors.nickname && touched.nickname && errors.nickname}
+                </div>
+              </div>
+              <div className={styles.registration}>
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={values.password}
+                  className={styles.registration_input_password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <div className={styles.error}>
+                  {errors.password && touched.password && errors.password}
+                </div>
+              </div>
+              <button type="submit">Registration</button>
+            </form>
+          )}
+        </Formik>
       </>
     );
   }
